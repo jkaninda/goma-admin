a33b6dd2646b9f0cb98ba24e969d430e46948c2c
Jonas Kaninda <jonaskaninda@gmail.com>
2026-03-29 06:36:03 +0200

    omit route Security and HealthCheck if not defined

--- internal/docker/types.go
+++ internal/docker/types.go
@@ -5,18 +5,18 @@ type GomaConfig struct {
 }
 
 type Route struct {
-	Name           string           `yaml:"name" json:"name"`
-	Path           string           `yaml:"path" json:"path"`
-	Rewrite        string           `yaml:"rewrite,omitempty" json:"rewrite,omitempty"`
-	Priority       int              `yaml:"priority,omitempty" json:"priority,omitempty"`
-	Enabled        bool             `yaml:"enabled,omitempty" default:"true" json:"enabled,omitempty"`
-	Hosts          []string         `yaml:"hosts,omitempty" json:"hosts,omitempty"`
-	Methods        []string         `yaml:"methods,omitempty" json:"methods,omitempty"`
-	Target         string           `yaml:"target,omitempty" json:"target,omitempty"`
+	Name           string            `yaml:"name" json:"name"`
+	Path           string            `yaml:"path" json:"path"`
+	Rewrite        string            `yaml:"rewrite,omitempty" json:"rewrite,omitempty"`
+	Priority       int               `yaml:"priority,omitempty" json:"priority,omitempty"`
+	Enabled        bool              `yaml:"enabled,omitempty" default:"true" json:"enabled,omitempty"`
+	Hosts          []string          `yaml:"hosts,omitempty" json:"hosts,omitempty"`
+	Methods        []string          `yaml:"methods,omitempty" json:"methods,omitempty"`
+	Target         string            `yaml:"target,omitempty" json:"target,omitempty"`
 	HealthCheck    *RouteHealthCheck `yaml:"healthCheck,omitempty" json:"healthCheck,omitempty"`
-	Security       *Security        `yaml:"security,omitempty" json:"security,omitempty"`
-	DisableMetrics bool             `yaml:"disableMetrics,omitempty" json:"disableMetrics,omitempty"`
-	Middlewares    []string         `yaml:"middlewares,omitempty" json:"middlewares,omitempty"`
+	Security       *Security         `yaml:"security,omitempty" json:"security,omitempty"`
+	DisableMetrics bool              `yaml:"disableMetrics,omitempty" json:"disableMetrics,omitempty"`
+	Middlewares    []string          `yaml:"middlewares,omitempty" json:"middlewares,omitempty"`
 }
 
 type RouteHealthCheck struct {
